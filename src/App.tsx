ebdb8de3edc2bf5318eb1fb8f07bbe9cc2397f16
Abu Raihan <aburaihanrahmani@gmail.com>
2025-07-01 05:06:17 -0700

    Write Actions in Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <>
       <h1>Redux practice</h1>
 
-      <button></button>
+      <button onClick={handleIncrement}>increment</button>
+      <div>0</div>
+      <button onClick={handleDecrement}>decrement</button>
     </>
   );
 }
